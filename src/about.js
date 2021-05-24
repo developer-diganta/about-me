f25f706f2850456f896b7052db5db1a9b72956c0
developer-diganta <digantabanik2000@gmail.com>
2021-05-24 19:28:44 +0530

    Updates

--- src/about.js
+++
@@ -1,6 +0,0 @@
-let about=`Hello! I am Diganta Kr Banik, currently a sophomore at Maulana Abul Kalam Azad University Of Technology. I love exploring 
-stuff related to computer science and have knack in coding. I have an interest in web devlopment and am currently also learning
-about Machine Learning. At other times, I am just a guy chilling out, listening to some soothing melodies on Spotify or maybe watching
-something on Youtube! Would love to connect!`
-
-export default about;
