c2bf2aa7efdad1a8f6bd625aaa4c857ed099c7fb
developer-diganta <digantabanik2000@gmail.com>
2021-05-28 18:19:05 +0530

    Updates

---
+++ static/js/3.0f8d22b6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[3],{414:function(p,s,a){}}]);
+//# sourceMappingURL=3.0f8d22b6.chunk.js.map
--- static/js/3.21756522.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[3],{413:function(p,s,a){}}]);
-//# sourceMappingURL=3.21756522.chunk.js.map
