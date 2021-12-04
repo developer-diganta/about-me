94df614e33709bf370cb828a44492f041072f428
Diganta Kr Banik <digantabanik2000@gmail.com>
2021-12-04 15:49:14 +0530

    Updates

--- static/js/3.0fe9421d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[3],{416:function(p,s,a){}}]);
-//# sourceMappingURL=3.0fe9421d.chunk.js.map
---
+++ static/js/3.23e64ca9.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[3],{514:function(p,s,a){}}]);
+//# sourceMappingURL=3.23e64ca9.chunk.js.map
